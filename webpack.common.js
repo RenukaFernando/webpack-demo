b64f001385a27fc64ffe653a1ee6ad7a987b65f2
Renuka Fernando <renukapiyumal@gmail.com>
2018-02-15 15:30:04 +0530

    babel-minify-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './development/src/index.js',
+    entry: {
+        index:'./development/src/index.js'
+    },
 
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
